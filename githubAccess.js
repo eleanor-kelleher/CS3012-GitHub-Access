e69139756ac805079c2f73296a104e1d3d62e2f8
James Kelleher <thisisjameskelleher@gmail.com>
2018-11-26 11:54:52 +0000

    Writing parsed data to JSON file

--- githubAccess.js
+++ githubAccess.js
@@ -1,7 +1,16 @@
 var github = require('octonode');
 
 var client = github.client();
+var fs = require("fs");
+var jsonData;
 
-client.get('/users/jameskhere', {}, function (err, status, body, headers) {
+client.get('/users/fabpot', {}, function (err, status, body, headers) {
   console.log(body); //json object
+  fs.writeFile("./data.json", JSON.stringify(body), (error) => {
+      if (error) {
+          console.error(err);
+          return;
+      };
+      console.log("Saved to JSON file\n");
+  });
 });
