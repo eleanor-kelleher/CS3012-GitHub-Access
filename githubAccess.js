0e3e68351e6cccca3c3ecbf9f938160a03561f50
James Kelleher <thisisjameskelleher@gmail.com>
2018-11-26 16:23:29 +0000

    user, repo, commits, contributors

--- githubAccess.js
+++ githubAccess.js
@@ -1,16 +1,52 @@
 var github = require('octonode');
 
 var client = github.client();
-var fs = require("fs");
-var jsonData;
+var fs = require('fs');
 
-client.get('/users/fabpot', {}, function (err, status, body, headers) {
-  console.log(body); //json object
-  fs.writeFile("./data.json", JSON.stringify(body), (error) => {
+//pcottle
+client.get('/users/pcottle', {}, function (err, status, body, headers) {
+  //console.log(body);
+  fs.writeFile("./pcottle.json", JSON.stringify(body), (error) => {
       if (error) {
           console.error(err);
           return;
       };
-      console.log("Saved to JSON file\n");
+      console.log("\npcottle JSON file\n");
+  });
+});
+
+//repo
+client.get('/repos/pcottle/learnGitBranching', {}, function (err, status, body, headers) {
+  //console.log(body);
+  fs.writeFile("./learnGitBranching.json", JSON.stringify(body), (error) => {
+      if (error) {
+          console.error(err);
+          return;
+      };
+      console.log("\nlearnGitBranching to JSON file\n");
+  });
+});
+
+//commits
+client.get('/repos/pcottle/learnGitBranching/commits', {}, function (err, status, body, headers) {
+  //console.log(body);
+  fs.writeFile("./commits.json", JSON.stringify(body), (error) => {
+      if (error) {
+          console.error(err);
+          return;
+      };
+      console.log("\ncommits to JSON file\n");
+  });
+});
+
+//contributors
+client.get('/repos/pcottle/learnGitBranching/contributors', {}, function (err, status, body, headers) {
+  //console.log(body);
+  fs.writeFile("./contributors.json", JSON.stringify(body), (error) => {
+      if (error) {
+          console.error(err);
+          return;
+      };
+      console.log("\ncontributors to JSON file\n");
   });
 });
