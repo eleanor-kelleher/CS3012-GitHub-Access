3b4c3e87fb67c303c529b53aae42d8c8bf769ee2
James Kelleher <thisisjameskelleher@gmail.com>
2018-11-27 17:46:06 +0000

    Getting ORGS and USERS & comparing contributions

--- githubAccess.js
+++ githubAccess.js
@@ -3,21 +3,60 @@ var github = require('octonode');
 var client = github.client();
 var fs = require('fs');
 
-// minimaxir - big-list-of-naughty-strings - contributors
-client.get('/repos/minimaxir/big-list-of-naughty-strings/contributors', {},
+// ORGANISATION vuejs - vue
+client.get('repos/vuejs/jp.vuejs.org/contributors', {},
 function (err, status, body, headers) {
-  fs.appendFileSync('./relevantinfo/wagoodman-contributors',
+  fs.appendFileSync('./relevantinfo/vue-contributors',
     "login,contributions\n");
   for(var x in body) {
     if(body.hasOwnProperty(x))
-      fs.appendFileSync('./relevantinfo/wagoodman-contributors', body[x].login
+      fs.appendFileSync('./relevantinfo/vue-contributors', body[x].login
+        + "," + body[x].contributions + "\n");
+  }
+  //console.log(body);
+});
+
+// ORGANISATION adobe - brackets-app
+client.get('/repos/adobe/brackets-app/contributors', {},
+function (err, status, body, headers) {
+  fs.appendFileSync('./relevantinfo/adobe-contributors',
+    "login,contributions\n");
+  for(var x in body) {
+    if(body.hasOwnProperty(x))
+      fs.appendFileSync('./relevantinfo/adobe-contributors', body[x].login
+        + "," + body[x].contributions + "\n");
+  }
+  //console.log(body);
+});
+
+//ORGANISATION firecracker-microvm/firecracker
+client.get('/repos/firecracker-microvm/firecracker/contributors', {},
+function (err, status, body, headers) {
+  fs.appendFileSync('./relevantinfo/firecracker-contributors',
+    "login,contributions\n");
+  for(var x in body) {
+    if(body.hasOwnProperty(x))
+      fs.appendFileSync('./relevantinfo/firecracker-contributors', body[x].login
         + "," + body[x].contributions + "\n");
   }
   //console.log(body);
 });
 
 
-// wagoodman - dive - contributors
+// USER minimaxir - big-list-of-naughty-strings
+client.get('/repos/minimaxir/big-list-of-naughty-strings/contributors', {},
+function (err, status, body, headers) {
+  fs.appendFileSync('./relevantinfo/minimaxir-contributors',
+    "login,contributions\n");
+  for(var x in body) {
+    if(body.hasOwnProperty(x))
+      fs.appendFileSync('./relevantinfo/minimaxir-contributors', body[x].login
+        + "," + body[x].contributions + "\n");
+  }
+  //console.log(body);
+});
+
+// USER wagoodman - dive
 client.get('/repos/wagoodman/dive/contributors', {}, function (err, status,
   body, headers) {
   fs.appendFileSync('./relevantinfo/wagoodman-contributors',
@@ -30,7 +69,7 @@ client.get('/repos/wagoodman/dive/contributors', {}, function (err, status,
   //console.log(body);
 });
 
-// pcottle - learnGitBranching - contributors
+// USER pcottle - learnGitBranching
 client.get('/repos/pcottle/learnGitBranching/contributors', {}, function (err,
   status, body, headers) {
   fs.appendFileSync('./relevantinfo/pcottle-contributors',
