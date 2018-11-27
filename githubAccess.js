5718fe55971d41cf3dbdcffc947e33e36b772f2c
James Kelleher <thisisjameskelleher@gmail.com>
2018-11-27 13:02:23 +0000

    json files now go to jsondata folder

--- githubAccess.js
+++ githubAccess.js
@@ -5,48 +5,48 @@ var fs = require('fs');
 
 //pcottle
 client.get('/users/pcottle', {}, function (err, status, body, headers) {
-  //console.log(body);
-  fs.writeFile("./pcottle.json", JSON.stringify(body), (error) => {
+  console.log(body);
+  fs.writeFile("./jsondata/pcottle.json", JSON.stringify(body), (error) => {
       if (error) {
           console.error(err);
           return;
       };
-      console.log("\npcottle JSON file\n");
+      console.log("pcottle JSON file\n");
   });
 });
 
 //repo
 client.get('/repos/pcottle/learnGitBranching', {}, function (err, status, body, headers) {
-  //console.log(body);
-  fs.writeFile("./learnGitBranching.json", JSON.stringify(body), (error) => {
+  console.log(body);
+  fs.writeFile("./jsondata/learnGitBranching.json", JSON.stringify(body), (error) => {
       if (error) {
           console.error(err);
           return;
       };
-      console.log("\nlearnGitBranching to JSON file\n");
+      console.log("learnGitBranching to JSON file\n");
   });
 });
 
 //commits
 client.get('/repos/pcottle/learnGitBranching/commits', {}, function (err, status, body, headers) {
-  //console.log(body);
-  fs.writeFile("./commits.json", JSON.stringify(body), (error) => {
+  console.log(body);
+  fs.writeFile("./jsondata/commits.json", JSON.stringify(body), (error) => {
       if (error) {
           console.error(err);
           return;
       };
-      console.log("\ncommits to JSON file\n");
+      console.log("commits to JSON file\n");
   });
 });
 
 //contributors
 client.get('/repos/pcottle/learnGitBranching/contributors', {}, function (err, status, body, headers) {
-  //console.log(body);
-  fs.writeFile("./contributors.json", JSON.stringify(body), (error) => {
+  console.log(body);
+  fs.writeFile("./jsondata/contributors.json", JSON.stringify(body), (error) => {
       if (error) {
           console.error(err);
           return;
       };
-      console.log("\ncontributors to JSON file\n");
+      console.log("contributors to JSON file\n");
   });
 });
