85f54cdea4f6c693d568e7f2702b4fcbd57cba74
James Kelleher <thisisjameskelleher@gmail.com>
2018-11-29 16:33:50 +0000

    Changes the format to be usable in the html file.

--- githubAccess.js
+++ githubAccess.js
@@ -6,12 +6,12 @@ var fs = require('fs');
 // ORGANISATION vuejs - vue
 client.get('repos/vuejs/jp.vuejs.org/contributors', {},
 function (err, status, body, headers) {
-  fs.appendFileSync('./relevantinfo/vue-contributors.csv',
-    "login,contributions\n");
+  fs.appendFileSync('./relevantinfo/vue-contributors.txt',"");
   for(var x in body) {
     if(body.hasOwnProperty(x))
-      fs.appendFileSync('./relevantinfo/vue-contributors.csv',
-      body[x].login + "," + body[x].contributions + "\n");
+      fs.appendFileSync('./relevantinfo/vue-contributors.txt',
+      "{\"login\":\"" + body[x].login + ",\"contributions\":"
+      +  body[x].contributions + "\"},\n");
   }
   //console.log(body);
 });
@@ -19,12 +19,12 @@ function (err, status, body, headers) {
 // ORGANISATION adobe - brackets-app
 client.get('/repos/adobe/brackets-app/contributors', {},
 function (err, status, body, headers) {
-  fs.appendFileSync('./relevantinfo/adobe-contributors.csv',
-    "login,contributions\n");
+  fs.appendFileSync('./relevantinfo/adobe-contributors.txt',"");
   for(var x in body) {
     if(body.hasOwnProperty(x))
-      fs.appendFileSync('./relevantinfo/adobe-contributors.csv',
-      body[x].login + "," + body[x].contributions + "\n");
+      fs.appendFileSync('./relevantinfo/adobe-contributors.txt',
+      "{\"login\":\"" + body[x].login + ",\"contributions\":"
+      +  body[x].contributions + "\"},\n");
   }
   //console.log(body);
 });
@@ -32,12 +32,12 @@ function (err, status, body, headers) {
 //ORGANISATION firecracker-microvm/firecracker
 client.get('/repos/firecracker-microvm/firecracker/contributors', {},
 function (err, status, body, headers) {
-  fs.appendFileSync('./relevantinfo/firecracker-contributors.csv',
-    "login,contributions\n");
+  fs.appendFileSync('./relevantinfo/firecracker-contributors.txt',"");
   for(var x in body) {
     if(body.hasOwnProperty(x))
-      fs.appendFileSync('./relevantinfo/firecracker-contributors.csv',
-      body[x].login + "," + body[x].contributions + "\n");
+      fs.appendFileSync('./relevantinfo/firecracker-contributors.txt',
+      "{\"login\":\"" + body[x].login + ",\"contributions\":"
+      +  body[x].contributions + "\"},\n");
   }
   //console.log(body);
 });
@@ -46,12 +46,12 @@ function (err, status, body, headers) {
 // USER minimaxir - big-list-of-naughty-strings
 client.get('/repos/minimaxir/big-list-of-naughty-strings/contributors', {},
 function (err, status, body, headers) {
-  fs.appendFileSync('./relevantinfo/minimaxir-contributors.csv',
-    "login,contributions\n");
+  fs.appendFileSync('./relevantinfo/minimaxir-contributors.txt',"");
   for(var x in body) {
     if(body.hasOwnProperty(x))
-      fs.appendFileSync('./relevantinfo/minimaxir-contributors.csv',
-      body[x].login + "," + body[x].contributions + "\n");
+      fs.appendFileSync('./relevantinfo/minimaxir-contributors.txt',
+      "{\"login\":\"" + body[x].login + ",\"contributions\":"
+      +  body[x].contributions + "\"},\n");
   }
   //console.log(body);
 });
@@ -59,12 +59,12 @@ function (err, status, body, headers) {
 // USER wagoodman - dive
 client.get('/repos/wagoodman/dive/contributors', {}, function (err, status,
   body, headers) {
-  fs.appendFileSync('./relevantinfo/wagoodman-contributors.csv',
-    "login,contributions\n");
+  fs.appendFileSync('./relevantinfo/wagoodman-contributors.txt',"");
   for(var x in body) {
     if(body.hasOwnProperty(x))
-      fs.appendFileSync('./relevantinfo/wagoodman-contributors.csv',
-      body[x].login  + "," + body[x].contributions + "\n");
+      fs.appendFileSync('./relevantinfo/wagoodman-contributors.txt',
+      "{\"login\":\"" + body[x].login + ",\"contributions\":"
+      +  body[x].contributions + "\"},\n");
   }
   //console.log(body);
 });
@@ -72,12 +72,12 @@ client.get('/repos/wagoodman/dive/contributors', {}, function (err, status,
 // USER pcottle - learnGitBranching
 client.get('/repos/pcottle/learnGitBranching/contributors', {}, function (err,
   status, body, headers) {
-  fs.appendFileSync('./relevantinfo/pcottle-contributors.csv',
-    "login,contributions\n");
+  fs.appendFileSync('./relevantinfo/pcottle-contributors.txt',"");
   for(var x in body) {
     if(body.hasOwnProperty(x))
-      fs.appendFileSync('./relevantinfo/pcottle-contributors.csv',
-      body[x].login + "," + body[x].contributions + "\n");
+      fs.appendFileSync('./relevantinfo/pcottle-contributors.txt',
+      "{\"login\":\"" + body[x].login + ",\"contributions\":"
+      +  body[x].contributions + "\"},\n");
   }
   //console.log(body);
 });
